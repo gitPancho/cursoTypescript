cc0406ad76ac3f7963081751afc274136bb4c189
GITPancho <f.valdes@outlook.cl>
2025-08-24 19:49:23 -0400

    ⚡️ Namespaces
    Namespace, imports y Exports

---
+++ ts-node/src/classes/hero.ts
@@ -0,0 +1,10 @@
+export class Hero {
+    constructor(public name: string, public age: number, public power: string) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+    }
+}
+
+export class Hero2 {}
+export class Hero3 {}
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,10 +1,8 @@
+import * as HeroClasses from './classes/hero';
+import powers from './data/powers';
 
+const Hero = 123;
+const spiderman = new HeroClasses.Hero('Spiderman', 30, 'Super');
 
-console.log('Hola Mundo!');
-
-
-
-
-
-
-
+console.log(spiderman);
+console.log(powers);
